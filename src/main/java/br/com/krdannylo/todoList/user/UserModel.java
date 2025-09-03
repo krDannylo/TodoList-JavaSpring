d849728b7958d406c44e8b0bc3afc657fb89bc5b
Dannylo Torres <krdannylo.new@gmail.com>
2025-09-03 14:54:00 -0300

    refactor: adds project files

---
+++ src/main/java/br/com/krdannylo/todoList/user/UserModel.java
@@ -0,0 +1,12 @@
+package br.com.krdannylo.todoList.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+
+  private String name;
+  private String username;
+  private String password;
+
+}
