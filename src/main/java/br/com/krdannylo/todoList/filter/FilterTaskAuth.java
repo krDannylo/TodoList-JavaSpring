68a108b2520a2b1e71666216cd66305a71bdbbe9
Dannylo Torres <krdannylo.new@gmail.com>
2025-09-09 17:08:50 -0300

    feat: adds FilterTaskAuth and TaskController

---
+++ src/main/java/br/com/krdannylo/todoList/filter/FilterTaskAuth.java
@@ -0,0 +1,67 @@
+package br.com.krdannylo.todoList.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.krdannylo.todoList.user.IUserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+  @Autowired
+  IUserRepository userRepository;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+
+    var servletPath = request.getServletPath();
+    if (!servletPath.equals("/tasks/")) {
+      filterChain.doFilter(request, response);
+      return;
+    }
+
+    var authorization = request.getHeader("Authorization");
+    if (authorization == null || !authorization.startsWith("Basic ")) {
+      response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authorization header not found");
+      return;
+    }
+    try {
+      var authEncoded = authorization.substring("Basic".length()).trim();
+      byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+
+      var authString = new String(authDecoded);
+
+      String[] credentials = authString.split(":");
+      String username = credentials[0];
+      String password = credentials[1];
+
+      var user = this.userRepository.findByUsername(username);
+      if (user == null) {
+        response.sendError(401);
+        return;
+      }
+
+      var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+      if (passwordVerify.verified == false) {
+        response.sendError(401);
+        return;
+      }
+
+      request.setAttribute("userId", user.getId());
+      filterChain.doFilter(request, response);
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      response.sendError(401);
+    }
+  }
+}
---
+++ src/main/java/br/com/krdannylo/todoList/task/TaskController.java
@@ -0,0 +1,29 @@
+package br.com.krdannylo.todoList.task;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+  @Autowired
+  private ITaskRepository taskRepository;
+
+  @PostMapping("/")
+  public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    var userId = request.getAttribute("userId");
+    taskModel.setUserId((UUID) userId);
+    var task = this.taskRepository.save(taskModel);
+    return task;
+  }
+
+}
